b3faf8edf303582e39cbc953dcc78b9785fbef8e
NaMToK14 <anhviet.ngo@gmail.com>
2026-06-12 23:29:57 +0200

    feat: github pages deployment information

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // Deine GitHub Pages Haupt-URL
+  site: 'https://NaMToK19.github.io',
+  // Der Name deines Repositories (mit Schrägstrich am Anfang!)
+  base: '/mh-nails-website',
   vite: {
     plugins: [tailwindcss()]
   },
